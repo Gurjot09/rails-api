ef2921c3dfd9956ed737c9abe09b80b39423a80c
gurjot@maropost.com <gurjot@maropost.com>
2018-10-04 21:27:00 +0000

    create method added

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,9 +1,23 @@
-module api
-	module v1
+module Api
+	module V1
 		class ArticlesController < ApplicationController
 			def index
 				articles = Article.order('created_at DESC')
 				render json: {status: 'SUCCESS', message: 'Loaded Articles', data: articles}, status: :ok
+			end
+
+			def show
+				article = Article.find(params[:id])
+				render json: {status: 'SUCCESS', message: 'Loaded Article', data: article}, status: :ok
+			end
+		
+			def create
+				article = Aricle.new(valid_params)
+			end
+		
+			private
+			def valid_params
+				params.permit(:title , :body)
 				
 			end
 		end
