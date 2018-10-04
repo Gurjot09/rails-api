71ef2e4f1d69f6140f4eeb038c12dfae00859ae8
gurjot@maropost.com <gurjot@maropost.com>
2018-10-04 21:48:30 +0000

    show action added

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,10 +1,30 @@
-module api
-	module v1
+module Api
+	module V1
 		class ArticlesController < ApplicationController
 			def index
 				articles = Article.order('created_at DESC')
 				render json: {status: 'SUCCESS', message: 'Loaded Articles', data: articles}, status: :ok
-				
+			end
+
+			def show
+				article = Article.find(params[:id])
+				render json: {status: 'SUCCESS', message: 'Loaded Article', data: article}, status: :ok
+			end
+		
+			def create
+				byebug
+				article = Article.new(valid_params)
+				if article.save
+					render json: {status: 'SUCCESS', message: 'Saved Article', data: article}, status: :ok
+				else
+					render json: {status: 'ERROR' , message: 'article not found' , data: article.errors},status: :unprocessable_entity
+					
+				end
+			end
+		
+			private
+			def valid_params
+				params.permit(:title , :body)
 			end
 		end
 	end
